848096f6c3925a98e080ae19674abcc3ba30397d
Kidus-Sofonias <sofoniaskidus@gmail.com>
2025-02-13 19:04:33 +0300

    axios fix

--- netflix/src/utilities/axios.js
+++ netflix/src/utilities/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://api.themoviedb.org/3, // Replace with your API base URL
+    baseURL: 'https://api.themoviedb.org/3'
 });
 
 export default instance;
