ca2151ab76886eed4d5a31589452e4852c4c8b4d
Kidus-Sofonias <sofoniaskidus@gmail.com>
2025-02-17 19:00:07 +0300

    final commit

--- netflix/src/components/Rows/Row/Row.jsx
+++ netflix/src/components/Rows/Row/Row.jsx
@@ -1,10 +1,13 @@
 import React, {useEffect, useState} from 'react'
 import './Row.css'
 import axios from '../../../utilities/axios'
+import movieTrailer from 'movie-trailer'
+import YouTube from 'react-youtube'
 
 
 const Row = ({title, fetchUrl, isLargeRow}) => {
     const [movies, setMovies] = useState([])
+    const [trailerUrl, setTrailerUrl] = useState('')
     const base_url = "https://image.tmdb.org/t/p/original/"
 
     useEffect(() => {
@@ -17,6 +20,17 @@ const Row = ({title, fetchUrl, isLargeRow}) => {
         }
         })();    
     }, [fetchUrl])
+    const handleClick = (movie) => {
+        if (trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTrailer(movie?.title || movie?.name || movie?.original_name )
+            .then(url => {
+                const urlParams = new URLSearchParams(new URL(url).search)
+                setTrailerUrl(urlParams.get('v'))
+            }).catch(error => console.log(error))
+    }
+    }
     const opts ={
         height: '390',
         width: '100%',
@@ -38,9 +52,9 @@ const Row = ({title, fetchUrl, isLargeRow}) => {
                 />
             ))}
         </div>
-        {/* <div style={{padding: '40px'}}>
+        <div style={{padding: '40px'}}>
             {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
-        </div> */}
+        </div> 
     </div>
   )
 }
