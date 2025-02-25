4e152b19ed6cb8a27cf015d90c14d92b703f2c68
Kidus-Sofonias <sofoniaskidus@gmail.com>
2025-02-25 18:17:47 +0300

    desciption

--- netflix/src/components/Rows/Row/Row.jsx
+++ netflix/src/components/Rows/Row/Row.jsx
@@ -39,21 +39,21 @@ const Row = ({title, fetchUrl, isLargeRow}) => {
         },
     }
     return (
-    <div className='row'>
-        <h1 className='title'>{title}</h1>
-        <div className="row-posters">
-            {movies?.map((movie, i) => (
+    <div className='row'> {/* Container for the entire row */}
+        <h1 className='title'>{title}</h1> {/* Display the title of the row */}
+        <div className="row-posters"> {/* Container for the movie posters */}
+            {movies?.map((movie, i) => ( /* Iterate over the movies array and render each movie poster */
                 <img
-                    onClick={() => handleClick(movie)}
-                    key={i}
-                    className={`row-poster ${isLargeRow && "row-poster-large"}`}
-                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} 
-                    alt={movie.name}
+                    onClick={() => handleClick(movie)} /* Set up click handler to play trailer */
+                    key={i} /* Unique key for each movie poster */
+                    className={`row-poster ${isLargeRow && "row-poster-large"}`} /* Apply CSS classes conditionally */
+                    src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} /* Set the image source URL */
+                    alt={movie.name} /* Set the alt text for the image */
                 />
             ))}
         </div>
-        <div style={{padding: '40px'}}>
-            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+        <div style={{padding: '40px'}}> {/* Padding around the YouTube player */}
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />} {/* Render YouTube player if trailerUrl is set */}
         </div> 
     </div>
   )
